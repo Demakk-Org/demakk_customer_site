e3b48fb33403709556b31269a028fa3845f7429f
ermias alehegn <ermiaseat1987@gmail.com>
2024-04-20 15:27:20 +0300

    DB-12 feeding data

---
+++ src/features/ProductList/components/ShippingChoice.tsx
@@ -0,0 +1,41 @@
+import { Stack, Typography } from '@mui/material';
+import React from 'react';
+
+interface ShippingChoiceProps {
+  choice?: boolean;
+  freeShippingPrice?: number;
+}
+
+export default function ShippingChoice({
+  choice,
+  freeShippingPrice,
+}: ShippingChoiceProps) {
+  return (
+    <>
+      {choice && (
+        <Stack>
+          <Typography
+            sx={{
+              '.choice': {
+                minWidth: 'max-content',
+                p: '0rem .4rem ',
+                fontSize: '.65rem',
+                fontWeight: 'bold',
+                backgroundColor: 'demakkSecondary.main',
+                borderRadius: '.2rem',
+              },
+              minWidth: 'max-content',
+              fontSize: '.875rem',
+            }}
+          >
+            <Typography component={'span'} className="choice" mr={'.25rem'}>
+              {choice}
+            </Typography>
+            Free shipping
+          </Typography>
+          <Typography> over ETB{freeShippingPrice}</Typography>
+        </Stack>
+      )}
+    </>
+  );
+}

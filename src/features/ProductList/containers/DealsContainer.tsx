d7fb0633874ebfaff093ba4bce9fc682193fdfd1
ermias alehegn <ermiaseat1987@gmail.com>
2024-05-06 09:36:57 +0300

    DB-8 products-page

--- src/features/ProductList/containers/DealsContainer.tsx
+++ src/features/ProductList/containers/DealsContainer.tsx
@@ -1,5 +1,8 @@
-import { Box, Divider, Stack, Typography } from '@mui/material';
-import React from 'react';
+import { Box, Divider, Stack, Typography } from "@mui/material";
+import React from "react";
+import useProductStore from "../../../store/product";
+import { Discount } from "../../../../dealsContainerStructure";
+import useDiscountStore from "@/store/discount";
 
 interface dealProps {
   deal: string | undefined;
@@ -12,45 +15,47 @@ export default function DealsContainer({
   extraDiscount,
   discountPercent,
 }: dealProps) {
+  const { product } = useProductStore();
+  const { discount } = useDiscountStore();
   return (
     <Stack
-      width={'100%'}
-      direction={'row'}
-      alignItems={'center'}
+      width={"100%"}
+      direction={"row"}
+      alignItems={"center"}
       divider={
         <Box
-          height={'.3rem'}
-          minWidth={'.3rem'}
-          m={'0 .25rem 0 .25rem'}
-          borderRadius={'50%'}
-          sx={{ backgroundColor: 'text.price' }}
+          height={".3rem"}
+          minWidth={".3rem"}
+          m={"0 .25rem 0 .25rem"}
+          borderRadius={"50%"}
+          sx={{ backgroundColor: "text.price" }}
         ></Box>
       }
     >
       <Typography
-        minWidth={'max-content'}
-        fontSize={'.6rem'}
-        fontWeight={'bold'}
-        color={'white'}
-        p={'0rem .4rem '}
-        borderRadius={'.2rem'}
-        sx={{ backgroundColor: 'error.main' }}
+        minWidth={"max-content"}
+        fontSize={".6rem"}
+        fontWeight={"bold"}
+        color={"white"}
+        p={"0rem .4rem "}
+        borderRadius={".2rem"}
+        sx={{ backgroundColor: "error.main" }}
       >
-        {deal}
+        {product?.getDeals(discount)}
       </Typography>
       {extraDiscount && (
         <Typography
           title="Extra 2% off with discont"
           noWrap
-          color={'text.price'}
-          fontSize={'.7rem'}
-          fontWeight={'bold'}
+          color={"text.price"}
+          fontSize={".7rem"}
+          fontWeight={"bold"}
         >
           Extra {extraDiscount}% off with discont
         </Typography>
       )}
       {discountPercent && (
-        <Typography color={'text.price'} fontSize={'.7rem'} fontWeight={'bold'}>
+        <Typography color={"text.price"} fontSize={".7rem"} fontWeight={"bold"}>
           -{discountPercent}%
         </Typography>
       )}
